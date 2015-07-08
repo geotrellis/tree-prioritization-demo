cc236e640464b158945668cc6b23e47b72ad9913
Justin Walgran <jwalgran@azavea.com>
2015-07-08 14:20:15 -0700

    WIP extract spark fns to separate file
    IT was easier to draft the the spark functions inline with the original
    akka ones, but now this is cleaner.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,10 +12,9 @@ object Main {
 
     // create and start our the service actor
     val service =
-      system.actorOf(Props(classOf[ModelingServiceActor]), "opentreemap-modeling-service")
+      system.actorOf(Props(classOf[ModelingServiceSparkActor]), "opentreemap-modeling-service")
 
     // start a new HTTP server with the service actor as the handler
     IO(Http) ! Http.Bind(service, ServiceConfig.host, port = ServiceConfig.port)
   }
 }
-
--- src/main/scala/ModelingServiceSparkActor.scala
+++ src/main/scala/ModelingServiceSparkActor.scala
@@ -14,6 +14,15 @@ import geotrellis.engine.op.local._
 import geotrellis.engine.op.zonal.summary._
 import geotrellis.engine.render._
 import geotrellis.engine.stats._
+import geotrellis.spark._
+import geotrellis.spark.io.hadoop._
+import geotrellis.spark.utils._
+import geotrellis.spark.op.zonal.summary._
+import geotrellis.spark.op.local._
+import geotrellis.spark.op.stats._
+
+import org.apache.spark._
+import org.apache.hadoop.fs._
 
 import akka.actor.Actor
 import spray.routing.HttpService
@@ -27,21 +36,33 @@ import spray.json.JsonParser.ParsingException
 import com.vividsolutions.jts.{ geom => jts }
 
 
-object ModelingTypes {
-  // Map of layer names to selected values.
-  // Ex. { Layer1: [1, 2, 3], ... }
-  type LayerMaskType = Map[String, Array[Int]]
-}
-
-
-class ModelingServiceActor extends Actor with ModelingService {
+class ModelingServiceSparkActor extends Actor with ModelingServiceSpark {
   override def actorRefFactory = context
   override def receive = runRoute(serviceRoute)
 }
 
 
-trait ModelingServiceLogic {
+trait ModelingServiceSparkLogic {
   import ModelingTypes._
+
+  val DEFAULT_ZOOM = 0
+  val DATA_PATH = "data/catalog"
+
+  def catalogPath(implicit sc: SparkContext): Path = {
+    val localFS = new Path(System.getProperty("java.io.tmpdir")).getFileSystem(sc.hadoopConfiguration)
+    new Path(localFS.getWorkingDirectory, DATA_PATH)
+  }
+
+  def catalog(implicit sc: SparkContext): HadoopRasterCatalog = {
+    val conf = sc.hadoopConfiguration
+    val localFS = catalogPath.getFileSystem(sc.hadoopConfiguration)
+    val catalog = HadoopRasterCatalog(catalogPath)
+
+    println(s"Writing data to the catalog")
+    LocalHadoopCatalog.writeTiffsToCatalog(catalog, DATA_PATH)
+
+    catalog
+  }
 
   def createRasterSource(layer: String) =
     RasterSource(layer)
@@ -70,38 +91,38 @@ trait ModelingServiceLogic {
     }
   }
 
-  /** Convert `layerMask` map to list of filtered rasters.
+ /** Convert `layerMask` map to list of filtered rasters.
     * The result contains a raster for each layer specified,
     * and that raster only contains whitelisted values present
     * in the `layerMask` argument.
     */
-  def parseLayerMaskParam(layerMask: Option[LayerMaskType],
-                          extent: RasterExtent): Iterable[RasterSource] = {
+  def parseLayerMaskParam(implicit sc:SparkContext,
+                          layerMask: Option[LayerMaskType],
+                          rasterExtent: RasterExtent): Iterable[RasterRDD[SpatialKey]] = {
     layerMask match {
       case Some(masks: LayerMaskType) =>
         masks map { case (layerName, values) =>
-          createRasterSource(layerName, extent).localMap { z =>
+          catalog.query[SpatialKey]((layerName, DEFAULT_ZOOM))
+          .where(Intersects(rasterExtent.extent))
+          .toRDD
+          .localMap { z =>
             if (values contains z) z
             else NODATA
           }
+
         }
       case None =>
-        Seq[RasterSource]()
+        Seq[RasterRDD[SpatialKey]]()
     }
   }
 
   /** Combine multiple polygons into a single mask raster. */
-  def polyMask(polyMasks: Iterable[Polygon])(model: RasterSource): RasterSource = {
-    if (polyMasks.size > 0)
-      model.mask(polyMasks)
-    else
-      model
+  def polyMask(polyMasks: Iterable[Polygon])(model: RasterRDD[SpatialKey]): RasterRDD[SpatialKey] = {
+    // TODO: Pull in an updated Geotrellis when this is complete and merged
+    // https://github.com/zifeo/geotrellis/commit/d63608cb7d77c0358a5dd8118f6289d6d9366799
+    model
   }
 
-  /** Combine multiple rasters into a single raster.
-    * The resulting raster will contain values from `model` only at
-    * points that have values in every `layerMask`. (AND-like operation)
-    */
   def layerMask(layerMasks: Iterable[RasterSource])(model: RasterSource): RasterSource = {
     if (layerMasks.size > 0) {
       layerMasks.foldLeft(model) { (rs, mask) =>
@@ -115,8 +136,28 @@ trait ModelingServiceLogic {
     }
   }
 
+
+  /** Combine multiple rasters into a single raster.
+    * The resulting raster will contain values from `model` only at
+    * points that have values in every `layerMask`. (AND-like operation)
+    */
+  def layerMask(layerMasks: Iterable[RasterRDD[SpatialKey]])(model: RasterRDD[SpatialKey]): RasterRDD[SpatialKey] = {
+    if (layerMasks.size > 0) {
+      layerMasks.foldLeft(model) { (rdd, mask) =>
+        rdd.combineTiles(mask) { (rddTile, maskTile) =>
+          rddTile.combine(maskTile) { (z, maskValue) =>
+            if (isData(maskValue)) z
+            else NODATA
+          }
+        }
+      }
+    } else {
+      model
+    }
+  }
+
   /** Filter all values from `model` that are less than `threshold`. */
-  def thresholdMask(threshold: Int)(model: RasterSource): RasterSource = {
+  def thresholdMask(threshold: Int)(model: RasterRDD[SpatialKey]): RasterRDD[SpatialKey] = {
     if (threshold > NODATA) {
       model.localMap { z =>
         if (z >= threshold) z
@@ -128,69 +169,106 @@ trait ModelingServiceLogic {
   }
 
   /** Filter model by 1 or more masks. */
-  def applyMasks(model: RasterSource, masks: (RasterSource) => RasterSource*) = {
-    masks.foldLeft(model) { (rs, mask) =>
-      mask(rs)
+  def applyMasks(model: RasterRDD[SpatialKey], masks: (RasterRDD[SpatialKey]) => RasterRDD[SpatialKey]*) = {
+    masks.foldLeft(model) { (rdd, mask) =>
+      mask(rdd)
     }
   }
 
   /** Multiply `layers` by their corresponding `weights` and combine
     * them to form a single raster.
     */
-  def weightedOverlay(layers:Seq[String],
+  def weightedOverlay(implicit sc: SparkContext,
+                      layers:Seq[String],
                       weights:Seq[Int],
-                      rasterExtent: RasterExtent): RasterSource = {
+                      bounds:Extent): RasterRDD[SpatialKey] = {
     layers
       .zip(weights)
       .map { case (layer, weight) =>
-        createRasterSource(layer, rasterExtent)
-          .convert(TypeByte)
-          .localMultiply(weight)
-       }
+        val base = catalog.query[SpatialKey]((layer, DEFAULT_ZOOM))
+        val intersected = base.where(Intersects(bounds))
+        val rdd = intersected.toRDD
+        rdd.convert(TypeByte).localMultiply(weight)
+      }
       .localAdd
   }
 
   /** Return a class breaks operation for `model`. */
-  def getBreaks(model: RasterSource, numBreaks: Int) = {
+  def getBreaks(model: RasterRDD[SpatialKey], numBreaks: Int) = {
    model.classBreaks(numBreaks)
   }
 
+  /*
+
+I asked Eugene about what replaced RasterSource.renderPng when working
+with Spark. He gave some option
+
+1. It’s a TMS service and we have tiles pyramided at all zoom
+levels. At which point you’re not working with RDDs, you’re just
+pulling them out using tileReader and doing normal raster tile
+operations.
+
+2. It’s a WMS service and you’re doing something like
+`rdd.stitch.crop(extent)` stitch will turn an RDD into a single tile
+and then you need to crop it to your exact extent since the tile
+boundaries may not have lined up.
+
+3. 2 is kind of wasteful for many parallel requests for very small
+areas, so you actually do your calculation for a larger area, as a
+buffer, then buffer results (somewhere) and query them by extent and
+again stitch.
+
+Ultimately, we want option 1; pyramid our source rasters, pull tiles
+out with the tileReader, and use "regular" raster processing and
+rendering. I am implementing the "stitch" version until we have
+pyramided data sources available.
+
+   */
   /** Return a render tile as PNG operation for `model`. */
-  def renderTile(model: RasterSource, breaks: Seq[Int], colorRamp: String): ValueSource[Png] = {
+  def renderTile(model: RasterRDD[SpatialKey], breaks: Seq[Int], colorRamp: String): Png = {
     val ramp = {
       val cr = ColorRampMap.getOrElse(colorRamp, ColorRamps.BlueToRed)
       cr.interpolate(breaks.length)
     }
-    model.renderPng(ramp.toArray, breaks.toArray)
+    model.stitch.renderPng(ramp.toArray, breaks.toArray)
   }
 
   /** Return raster value distribution for `model`. */
-  def histogram(model: RasterSource, polyMask: Seq[Polygon]): ValueSource[Histogram] = {
+  def histogram(rdd: RasterRDD[SpatialKey], polyMask: Seq[Polygon]): Histogram = {
     if (polyMask.size > 0) {
-      val histograms =
-        DataSource.fromSources(
-          polyMask map { p => model.zonalHistogram(p) }
-        )
-      histograms.converge { seq => FastMapHistogram.fromHistograms(seq) }
+      val histograms: Seq[Histogram] = polyMask map {
+        p => {
+          println(s"POLYGON: $p")
+          rdd.zonalHistogram(p)
+        }
+      }
+      println(s"HISTOGRAMS: $histograms")
+      FastMapHistogram.fromHistograms(histograms)
     } else {
-      model.histogram
+      null
     }
   }
 
-  /** Return raster value at a certain point. */
-  def rasterValue(model: RasterSource, pt: Point): Int = {
-    model.mapWithExtent { (tile, extent) =>
-      if (extent.intersects(pt)) {
-        val re = RasterExtent(extent, tile.cols, tile.rows)
-        val (col, row) = re.mapToGrid(pt.x, pt.y)
-        Some(tile.get(col, row))
-      } else {
-        None
+  /** Return raster values for a sequence of points. */
+  def rasterValues(tileReader: SpatialKey => Tile, metadata: RasterMetaData)(points: Seq[(String, Point)]) : Seq[(String, Point, Int)] = {
+    val keyedPoints: Map[SpatialKey, Seq[(String, Point)]] =
+      points
+        .map { case(id, p) =>
+          // TODO, don't project if the raster is already in WebMercator
+          metadata.mapTransform(p.reproject(WebMercator, metadata.crs)) -> (id, p)
+        }
+        .groupBy(_._1)
+        .map { case(k, pairs) => k -> pairs.map(_._2) }
+
+    keyedPoints.map { case(k, points) =>
+      val raster = Raster(tileReader(k), metadata.mapTransform(k))
+      points.map { case(id, p) =>
+        // TODO, don't project if the raster is already in WebMercator
+        val projP = p.reproject(WebMercator, metadata.crs)
+        val (col, row) = raster.rasterExtent.mapToGrid(projP.x, projP.y)
+        (id, p, raster.tile.get(col,row))
       }
-    }.get.flatten.toList match {
-      case head :: tail => head
-      case Nil => NODATA
-    }
+    }.toSeq.flatten
   }
 
   def reprojectPolygons(polys: Seq[Polygon], srid: Int): Seq[Polygon] = {
@@ -210,20 +288,19 @@ trait ModelingServiceLogic {
   }
 }
 
-trait ModelingService extends HttpService with ModelingServiceLogic {
+trait ModelingServiceSpark extends HttpService with ModelingServiceSparkLogic {
   import ModelingTypes._
 
   implicit def executionContext = actorRefFactory.dispatcher
 
+  implicit val sparkContext = SparkUtils.createLocalSparkContext("local[8]", "Local Context", new SparkConf())
+
   lazy val serviceRoute =
     handleExceptions(exceptionHandler) {
-      indexRoute ~
-      colorsRoute ~
       breaksRoute ~
       overlayRoute ~
       histogramRoute ~
-      rasterValueRoute ~
-      staticRoute
+      rasterValueRoute
     }
 
   /** Handle all exceptions that bubble up from `failWith` calls. */
@@ -240,21 +317,7 @@ trait ModelingService extends HttpService with ModelingServiceLogic {
       complete(StatusCodes.InternalServerError)
   }
 
-  // TODO: Remove
-  lazy val indexRoute = pathSingleSlash {
-    getFromFile(ServiceConfig.staticPath + "/index.html")
-  }
-
-  // TODO: Remove
-  lazy val staticRoute = pathPrefix("") {
-    getFromDirectory(ServiceConfig.staticPath)
-  }
-
-  lazy val colorsRoute = path("gt" / "colors") {
-    complete(ColorRampMap.getJson)
-  }
-
-  lazy val breaksRoute = path("gt" / "breaks") {
+  lazy val breaksRoute = path("gt" / "spark" / "breaks") {
     post {
       formFields('bbox,
                  'layers,
@@ -288,33 +351,28 @@ trait ModelingService extends HttpService with ModelingServiceLogic {
             srid
           )
 
-          val unmasked = weightedOverlay(layers, weights, rasterExtent)
+          val unmasked = weightedOverlay(implicitly, layers, weights, rasterExtent.extent)
           val model = applyMasks(
             unmasked,
             polyMask(polys),
-            layerMask(parseLayerMaskParam(parsedLayerMask, rasterExtent)),
+            layerMask(parseLayerMaskParam(implicitly, parsedLayerMask, rasterExtent)),
             thresholdMask(threshold)
           )
 
           val breaks = getBreaks(model, numBreaks)
-          breaks.run match {
-            case Complete(breaks, _) =>
-              if (breaks.size > 0 && breaks(0) == NODATA) {
-                failWith(new ModelingException("Unable to calculate breaks (NODATA)."))
-              } else {
-                val breaksArray = breaks.mkString("[", ",", "]")
-                val json = s"""{ "classBreaks" : $breaksArray }"""
-                complete(json)
-              }
-            case Error(message, trace) =>
-              failWith(new RuntimeException(message))
+          if (breaks.size > 0 && breaks(0) == NODATA) {
+            failWith(new ModelingException("Unable to calculate breaks (NODATA)."))
+          } else {
+            val breaksArray = breaks.mkString("[", ",", "]")
+            val json = s"""{ "classBreaks" : $breaksArray }"""
+            complete(json)
           }
         }
       }
     }
   }
 
-  lazy val overlayRoute = path("gt" / "wo") {
+  lazy val overlayRoute = path("gt" / "spark" / "wo") {
     post {
       formFields('service,
                  'request,
@@ -339,6 +397,7 @@ trait ModelingService extends HttpService with ModelingServiceLogic {
           val rasterExtent = RasterExtent(extent, cols, rows)
 
           val layers = layersString.split(",")
+
           val weights = weightsString.split(",").map(_.toInt)
           val breaks = breaksString.split(",").map(_.toInt)
 
@@ -357,88 +416,89 @@ trait ModelingService extends HttpService with ModelingServiceLogic {
             srid
           )
 
-          val unmasked = weightedOverlay(layers, weights, rasterExtent)
+          val unmasked = weightedOverlay(implicitly, layers, weights, rasterExtent.extent)
           val model = applyMasks(
             unmasked,
             polyMask(polys),
-            layerMask(parseLayerMaskParam(parsedLayerMask, rasterExtent)),
+            layerMask(parseLayerMaskParam(implicitly, parsedLayerMask, rasterExtent)),
             thresholdMask(threshold)
           )
 
           val tile = renderTile(model, breaks, colorRamp)
-          tile.run match {
-            case Complete(img, h) =>
-              respondWithMediaType(MediaTypes.`image/png`) {
-                complete(img.bytes)
-              }
-            case Error(message, trace) =>
-              failWith(new RuntimeException(message))
+          respondWithMediaType(MediaTypes.`image/png`) {
+            complete(tile.bytes)
           }
         }
       }
     }
   }
 
-  lazy val histogramRoute = path("gt" / "histogram") {
+  lazy val histogramRoute = path("gt" / "spark"/ "histogram") {
     post {
-      formFields('bbox,
-                 'layer,
+      formFields('layer,
                  'srid.as[Int],
                  'polyMask ? "") {
-        (bbox, layer, srid, polyMaskParam) => {
+        (layer, srid, polyMaskParam) => {
           val start = System.currentTimeMillis()
-          val extent = Extent.fromString(bbox)
-          // TODO: Dynamic breaks based on configurable breaks resolution.
-          val rasterExtent = RasterExtent(extent, 256, 256)
-          val rs = createRasterSource(layer, rasterExtent)
+          val layerId = (layer, DEFAULT_ZOOM)
+          val metadata: RasterMetaData = catalog.getLayerMetadata(layerId).rasterMetaData
 
-          val polys = reprojectPolygons(
+          // TODO: dont double reproject
+          val webMPolys = reprojectPolygons(
             parsePolyMaskParam(polyMaskParam),
             srid
           )
+          val polys = webMPolys map { p => p.reproject(WebMercator, metadata.crs) }
 
-          val summary = histogram(rs, polys)
-          summary.run match {
-            case Complete(result, h) =>
-              val elapsedTotal = System.currentTimeMillis - start
-              val histogram = result.toJSON
-              val data =
-                s"""{
+          // TODO expand extent to include multiple polygons
+          val polygonsExtent = polys.head.envelope
+
+          println(s"POLYGONSEXTENT: $polygonsExtent")
+
+          val baseQuery = catalog.query[SpatialKey]((layer, DEFAULT_ZOOM))
+          val intersection = baseQuery.where(Intersects(polygonsExtent))
+          val rdd = intersection.toRDD
+
+          val result = histogram(rdd, polys)
+          val elapsedTotal = System.currentTimeMillis - start
+          val histogramJson = result.toJSON
+          val data =
+            s"""{
                   "elapsed": "$elapsedTotal",
-                  "histogram": $histogram
+                  "histogram": $histogramJson
                     }"""
-              complete(data)
-            case Error(message, trace) =>
-              failWith(new RuntimeException(message))
-          }
+          complete(data)
         }
       }
     }
   }
 
-  lazy val rasterValueRoute = path("gt" / "value") {
+  lazy val rasterValueRoute = path("gt" / "spark" / "value") {
     post {
       formFields('layer, 'coords, 'srid.as[Int]) {
         (layer, coordsParam, srid) =>
 
-        val rs = createRasterSource(layer)
-
-        val coords = (coordsParam.split(",").grouped(3) collect {
+        val points = coordsParam.split(",").grouped(3).map {
+          // TODO: Preserve ids through the processing
           case Array(id, xParam, yParam) =>
             try {
               val pt = reprojectPoint(
                 Point(xParam.toDouble, yParam.toDouble),
                 srid
               )
-              val z = rasterValue(rs, pt)
-              Some((id, pt.x, pt.y, z))
+              Some((id, pt))
             } catch {
               case ex: NumberFormatException => None
             }
-        }).toList
+        }.toList.flatten
+
+        val layerId = (layer, DEFAULT_ZOOM)
+
+        val values = rasterValues(catalog.tileReader[SpatialKey](layerId), catalog.getLayerMetadata(layerId).rasterMetaData)(points)
 
         import spray.json.DefaultJsonProtocol._
-        complete(s"""{ "coords": ${coords.toJson} }""")
+        // TODO: Return points with values
+        complete(s"""{ "coords": ${values.toJson} }""")
       }
     }
   }
