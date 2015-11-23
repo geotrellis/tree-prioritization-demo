e7a3ab018132f46d4626086d34ce2ce909f29ea6
Justin Walgran <jwalgran@azavea.com>
2015-11-23 13:47:24 -0700

    Add scripts to upload jars to Github releases
    The `upload-jar-to-release.sh` script is designed to be run on every
    Jenkins build of `otm-modeling` after the combined assembly is built. If
    the HEAD of the repo matches the most recent tag (sorted by commit date)
    the script creates a release for that tag and uploads the compiled jar.
    
    This script uses https://github.com/aktau/github-release to work with
    Github releases at a higher level of abstraction.
    
    I broke two smaller helpers out of the main script to ease debugging and testing.

---
+++ deployment/scripts/on-latest-tag.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Write the name of the latest tag to stdout if HEAD is equal to the
+# latest tag, else exit 1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+tagsha=`$DIR/latest-tag.sh | awk '{print $1;}' `
+tagname=`$DIR/latest-tag.sh | awk '{print $2;}' `
+headsha=`git rev-parse --verify HEAD`
+
+if [ "$tagsha" == "$headsha" ]; then echo $tagname; else echo "NOT_ON_THE_LATEST_TAG"; exit 1; fi
