d5bb50bf411d719754f777311c09794320698b47
RickMohr <rmohr@azavea.com>
2016-03-23 13:33:45 -0400

    Extract helper function getTileWithZoom

--- tile/src/main/scala/TileServiceLogic.scala
+++ tile/src/main/scala/TileServiceLogic.scala
@@ -11,13 +11,13 @@ import geotrellis.spark.io.Intersects
 import geotrellis.spark.io.index._
 import geotrellis.spark.io.s3._
 import geotrellis.spark.io.json._
-import geotrellis.spark.tiling._
+import geotrellis.spark.tiling._ //kill?
 import geotrellis.raster.resample._
 import geotrellis.raster.render._
 import geotrellis.spark.op.local._
 
 trait TileServiceLogic {
-  val DEFAULT_ZOOM = 11
+  val MAX_ZOOM = 11
   val BREAKS_ZOOM = 8
 
   def addTiles(t1: Tile, t2: Tile): Tile = {
@@ -29,6 +29,30 @@ trait TileServiceLogic {
       .attributeStore
       .readLayerAttributes[S3LayerHeader, RasterMetaData, KeyBounds[SpatialKey], KeyIndex[SpatialKey], Schema](layerId)._2
 
+  def getTileWithZoom(implicit sc: SparkContext,
+                      tileReader: S3TileReader[SpatialKey, Tile],
+                      layer:String,
+                      z:Int,
+                      x:Int,
+                      y:Int,
+                      maxZoom:Int): Tile = {
+    if (z <= maxZoom) {
+      val reader = tileReader.read(layer, z)
+      reader(SpatialKey(x, y))
+    } else {
+      val layerId = LayerId(layer, maxZoom)
+      val reader = tileReader.read(layerId)
+      val rmd = getMetadata(sc, tileReader, layerId)
+      val layoutLevel = ZoomedLayoutScheme(rmd.crs).levelForZoom(rmd.extent, z)
+      val mapTransform = MapKeyTransform(rmd.crs, layoutLevel.layout.layoutCols, layoutLevel.layout.layoutRows)
+      val targetExtent = mapTransform(x, y)
+      val gb @ GridBounds(nx, ny, _, _) = rmd.mapTransform(targetExtent)
+      val sourceExtent = rmd.mapTransform(nx, ny)
+      val largerTile = reader(SpatialKey(nx, ny))
+      largerTile.resample(sourceExtent, RasterExtent(targetExtent, 256, 256))
+    }
+  }
+
   def weightedOverlay(implicit sc: SparkContext,
                       tileReader: S3TileReader[SpatialKey, Tile],
                       layers:Seq[String],
@@ -36,28 +60,11 @@ trait TileServiceLogic {
                       z:Int,
                       x:Int,
                       y:Int): Tile = {
+    // TODO: Handle layers with different pyramids instead of using MAX_ZOOM
     layers
       .zip(weights)
       .map { case (layer, weight) =>
-        // TODO: Handle layers with different pyramids instead of using
-        // DEFAULT_ZOOM
-        val tile =
-          if (z <= DEFAULT_ZOOM) {
-            val reader = tileReader.read(layer, z)
-            reader(SpatialKey(x, y))
-          } else {
-            val layerId = LayerId(layer, DEFAULT_ZOOM)
-            val reader = tileReader.read(layerId)
-            val rmd = getMetadata(sc, tileReader, layerId)
-            val layoutLevel = ZoomedLayoutScheme(rmd.crs).levelForZoom(rmd.extent, z)
-            val mapTransform = MapKeyTransform(rmd.crs, layoutLevel.layout.layoutCols, layoutLevel.layout.layoutRows)
-            val targetExtent = mapTransform(x, y)
-            val gb @ GridBounds(nx, ny, _, _) = rmd.mapTransform(targetExtent)
-            val sourceExtent = rmd.mapTransform(nx, ny)
-            val largerTile = reader(SpatialKey(nx, ny))
-            largerTile.resample(sourceExtent, RasterExtent(targetExtent, 256, 256))
-          }
-        // Convert Byte tiles to Int so that math operations do not overflow
+        val tile = getTileWithZoom(sc, tileReader, layer, z, x, y, MAX_ZOOM)
         tile.convert(TypeInt).map(_ * weight)
     }
       .reduce(addTiles)
