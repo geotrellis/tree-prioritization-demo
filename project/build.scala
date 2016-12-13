84d01829c05837e1e96ab684e3507ce46b99c27e
RickMohr <rmohr@azavea.com>
2016-12-13 16:55:14 -0500

    1.4.0

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object Version {
   def either(environmentVariable: String, default: String): String =
     Properties.envOrElse(environmentVariable, default)
 
-  val modeling     = "1.3.0"
+  val modeling     = "1.4.0"
 
   val geotools     = "8.0-M4"
   val geotrellis   = "0.10.1"
